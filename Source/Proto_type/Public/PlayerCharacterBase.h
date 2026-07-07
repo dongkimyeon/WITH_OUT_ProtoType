404b449453e70c0dd2ac533bbc1c949293579151
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2026-07-07 18:38:15 +0900

    커스텀 폰 클래스로 SandboxChararacter_Mover 교체

--- Source/Proto_type/Public/PlayerCharacterBase.h
+++ Source/Proto_type/Public/PlayerCharacterBase.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
+#include "GameFramework/Pawn.h"
 #include "PlayerCharacterBase.generated.h"
 
 UCLASS()
-class PROTO_TYPE_API APlayerCharacterBase : public ACharacter
+class PROTO_TYPE_API APlayerCharacterBase : public APawn
 {
 	GENERATED_BODY()
 
