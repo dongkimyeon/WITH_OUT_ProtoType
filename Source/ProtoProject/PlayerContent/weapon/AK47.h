f0b6ceb0eee70802e99fbcddb56b69f8237d73f3
ToughGuyClub(laptab) <gyudong6185@gmail.com>
2026-07-09 01:07:14 +0900

    총 에셋및 c++블프준비

---
+++ Source/ProtoProject/PlayerContent/weapon/AK47.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AK47.generated.h"
+
+UCLASS()
+class PROTOPROJECT_API AAK47 : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AAK47();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+    UStaticMeshComponent* WeaponMesh;
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+};
