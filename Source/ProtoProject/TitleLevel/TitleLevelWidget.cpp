1cc7bccef0026e7b31563eb09e5bc08823498b3a
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2026-07-25 01:12:09 +0900

    TitleLevel 회원가입 로그인 인터페이스 추가

---
+++ Source/ProtoProject/TitleLevel/TitleLevelWidget.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TitleLevelWidget.h"
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+
+void UTitleLevelWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (LoginButton) LoginButton->OnClicked.AddDynamic(this, &UTitleLevelWidget::OnClickLogIn);
+	
+	if (SignInButton) SignInButton->OnClicked.AddDynamic(this, &UTitleLevelWidget::OnClickSignIn);
+}
+
+void UTitleLevelWidget::OnClickLogIn()
+{
+	if (Id_Input_field) FID = Id_Input_field->GetText().ToString();
+	if (Passwd_Input_field) FPassword = Passwd_Input_field->GetText().ToString();
+	const FString Msg = FString::Printf(TEXT("로그인 : ID: %s / PW: %s "),
+			*FID, *FPassword);
+	GEngine->AddOnScreenDebugMessage(-1,1.0f,FColor::Green, Msg);
+}
+
+void UTitleLevelWidget::OnClickSignIn()
+{
+	if (Id_Input_field) FID = Id_Input_field->GetText().ToString();
+	if (Passwd_Input_field) FPassword = Passwd_Input_field->GetText().ToString();
+	const FString Msg = FString::Printf(TEXT("회원가입 : ID: %s / PW: %s "),
+			*FID, *FPassword);
+	
+	GEngine->AddOnScreenDebugMessage(-1,1.0f,FColor::Red, Msg);
+}
+
+
---
+++ Source/ProtoProject/TitleLevel/TitleLevelWidget.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TitleLevelWidget.generated.h"
+
+class UButton;
+class UEditableText;
+UCLASS()
+class PROTOPROJECT_API UTitleLevelWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta = (BindWidgetOptional))
+	UButton* LoginButton;
+	
+	UPROPERTY(meta = (BindWidgetOptional))
+	UButton* SignInButton;
+	
+	UPROPERTY(meta = (BindWidgetOptional))
+	UEditableText* Id_Input_field;
+	
+	UPROPERTY(meta = (BindWidgetOptional))
+	UEditableText* Passwd_Input_field;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, category = Test)
+	FString FID;
+		
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, category = Test)
+	FString FPassword;
+	
+	UFUNCTION()
+	void OnClickLogIn();
+	
+	UFUNCTION()
+	void OnClickSignIn();
+	
+};
